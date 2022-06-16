2b1286a20b2e923f0db77cdbd676dc489b371d56
Suranjana Adhikari <adhikareesuranjana@gmail.com>
2022-06-16 20:08:54 +0545

    Second Commit

--- app/src/main/java/com/suranjana/mobileappdevelopment/MainActivity.java
+++ app/src/main/java/com/suranjana/mobileappdevelopment/MainActivity.java
@@ -2,13 +2,29 @@ package com.suranjana.mobileappdevelopment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+   //ahile kaha xau.this, aarko jane activity.class
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+
+      //          Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
